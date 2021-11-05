0241b6dfb482551beb47577e7ac36952e9817185
devcapt <mohammaddev7@gmail.com>
2021-11-05 12:06:49 +0330

    puse on hover & ability to interact implimented to the crousel

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,8 +1,15 @@
+// Swiper js
 var swiper = new Swiper(".mySwiper", {
   effect: "cards",
   grabCursor: true,
   autoplay: {
-    delay: 4500,
+    delay: 3500,
+    pauseOnMouseEnter:true,
+    disableOnInteraction:false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
   },
 });
-
+// Swiper js
