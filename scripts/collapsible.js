c83058e40794972ec798462e21a0d25bf4443d9b
devcapt <mohammaddev7@gmail.com>
2021-11-05 12:09:04 +0330

    create collapsible component

---
+++ scripts/collapsible.js
@@ -0,0 +1,8 @@
+// Collapsible
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+// Collapsible
